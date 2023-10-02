c9ab11372e42d5b35f71cc6eb8731247842d57e9
Thomas Smallwood <thomassmallwood@Thomass-MacBook-Pro.local>
2023-10-02 14:05:30 -0500

    prepare for heroku deployment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch-all route to serve 'index.html' for SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
